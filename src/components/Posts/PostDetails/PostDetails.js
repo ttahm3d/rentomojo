3da1aae3404a37d5c8fa8e652bc6d5d9ef052d95
Tahir Ahmed <53032124+ttahm3d@users.noreply.github.com>
2021-02-14 10:38:37 +0530

    fetching post id from post page

--- src/components/Posts/PostDetails/PostDetails.js
+++ src/components/Posts/PostDetails/PostDetails.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import axios from 'axios';
-import { useQuery } from 'react-query'
+import React from "react";
+import axios from "axios";
+import { useQuery } from "react-query";
 
-const getPostDetails = async () => {
-	await (await axios.get(`/posts/1`)
-		.then(({ data }) => data)
-	)
+const getPostDetails = async ({ queryKey }) => {
+  const [_key, { postId }] = queryKey;
+  const response = await axios.get(`/posts/${postId}`);
+  return response.data;
 };
 
 const PostDetails = (props) => {
-	const { data, isLoading, error } = useQuery("posts", getPostDetails);
-	console.log(data)
-	return (
-		<div>
-			<p>something</p>
-		</div>
-	)
-}
+  const postId = props.match.params.id;
+  const { data, isLoading, error } = useQuery(
+    ["posts", { postId }],
+    getPostDetails
+  );
+  console.log(data);
+  return <div></div>;
+};
 
 export default PostDetails;
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,30 +1,33 @@
-import React, { useState } from 'react';
-import { useQuery } from 'react-query';
-import Post from './Post/Post'
-import axios from 'axios'
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import Post from "./Post/Post";
+import axios from "axios";
 
-const getPostsByUserId = async ({queryKey}) => {
-	const [_key, {userId}] = queryKey;
-	const response = await axios.get(
-		`https://jsonplaceholder.typicode.com/posts?userId=${userId}`
-	);
-	return response.data;
+const getPostsByUserId = async ({ queryKey }) => {
+  const [_key, { userId }] = queryKey;
+  const response = await axios.get(`/posts?userId=${userId}`);
+  return response.data;
 };
 
 const Posts = (props) => {
-	const search = props.location.search; // could be '?foo=bar'
-	const params = new URLSearchParams(search);
-	const userId = params.get('userId');
-	const { data, isLoading, error } = useQuery(["posts", {userId}], getPostsByUserId)
-	
-	return (
-		<div>
-			{isLoading && <p>Loading</p>}
-			<ul>
-				{data && data.map(post => (<Post key={post.id} post={post} />))}
-			</ul>
-		</div>
-	)
-}
+  const [postId, setpostId] = useState(-1);
+
+  const search = props.location.search;
+  const params = new URLSearchParams(search);
+  const userId = params.get("userId");
+
+  const { data, isLoading, error } = useQuery(
+    ["posts", { userId }],
+    getPostsByUserId
+  );
+
+  return (
+    <div>
+      {isLoading && <p>Loading</p>}
+      <ul>{data && data.map((post) => <Post key={post.id} post={post} />)}</ul>
+    </div>
+  );
+};
 
 export default Posts;
